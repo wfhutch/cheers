558f356521c96c6937c8a70407a90b3a930a9ea0
wfhutch <wfh2727@gmail.com>
2015-10-13 18:16:25 -0500

    Added Cheers project

---
+++ Cheers/Cheers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello there! What's your name?");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("Hi, " + name);
+            foreach (char letter in name)
+            {
+                string anLetters = "halfnorsemixHALFNORSEMIX";
+                if (anLetters.IndexOf(letter) == -1)
+                {
+                    System.Console.WriteLine("Give me a .. " + letter);
+                    System.Console.ReadLine();
+                }
+                else
+                {
+                    System.Console.WriteLine("Give me an .. " + letter);
+                    System.Console.ReadLine();
+                }  
+            }
+            string newName = name.ToUpper();
+            System.Console.WriteLine(newName + " is.. GRAND!");
+            System.Console.WriteLine("Press any key to exit");
+            System.Console.ReadKey();
+        }
+    }
+}
